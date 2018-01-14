7e8fff0b3529762ad8ace4a4e1ef78580a98730f
Zak652 <m2kzak@gmail.com>
2018-01-14 23:07:16 +0300

    first commit for remote push

--- posts/database.py
+++ posts/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -8,3 +8,21 @@ engine = create_engine(app.config["DATABASE_URI"])
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(128))
+    body = Column(String(1024))
+
+    def as_dictionary(self):
+        post = {
+            "id": self.id,
+            "title": self.title,
+            "body": self.body
+        }
+        return post
+
+Base.metadata.create_all(engine)
